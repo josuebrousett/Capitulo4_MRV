716e298f0de4bfe79bbf8e5fa53fc9acb9d13ebb
josuebrousett <44821995+josuebrousett@users.noreply.github.com>
2019-05-09 08:44:28 -0500

    Add files via upload

---
+++ DestroyTimeOut.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimeOut : MonoBehaviour {
+	public float timer = 15.0f;
+	void Start () {
+		Destroy (gameObject, timer);
+	}
+}
